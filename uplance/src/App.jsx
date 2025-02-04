15420769873f5f890c2e664529e5b11185038d95
Anup0099 <anuppal602@gmail.com>
2025-02-04 21:27:07 +0530

    done

--- uplance/src/App.jsx
+++ uplance/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Button, Container, Stack } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link,Navigate } from "react-router-dom";
 
 import PrivateRoute from "./PrivateRoute";
 import PublicRoute from "./PublicRoutes";
@@ -11,78 +11,78 @@ import UserProfile from "./components/UserProfile";
 import UserEditor from "./components/UserEditor";
 import Login from "./pages/Login";
 import Navbar from "./pages/Navbar";
-
+import { useAuth } from "./AuthContext";
 function App() {
+  const { user } = useAuth()|| {};
   return (
-    
-      <Router>
-        <Navbar />
-        <Container>
-          {/* Navigation Buttons (Visible Always) */}
-          <Stack direction="row" spacing={2} sx={{ my: 3 }}>
-            <Button component={Link} to="/userForm" variant="contained">
-              User Form
-            </Button>
-            <Button component={Link} to="/editor" variant="contained">
-              Edit User
-            </Button>
-            <Button component={Link} to="/profile" variant="contained">
-              User Profile
-            </Button>
-            <Button component={Link} to="/counter" variant="contained">
-              Counter
-            </Button>
-          </Stack>
+    <Router>
+      <Navbar />
+      <Container>
+        {/* Navigation Buttons (Visible Always) */}
+        <Stack direction="row" spacing={2} sx={{ my: 3 }}>
+          <Button component={Link} to="/userForm" variant="contained">
+            User Form
+          </Button>
+          <Button component={Link} to="/editor" variant="contained">
+            Edit User
+          </Button>
+          <Button component={Link} to="/profile" variant="contained">
+            User Profile
+          </Button>
+          <Button component={Link} to="/counter" variant="contained">
+            Counter
+          </Button>
+        </Stack>
 
-          {/* Route Definitions */}
-          <Routes>
-            {/* Public Route: Only accessible when NOT logged in */}
-            <Route
-              path="/login"
-              element={
-                <PublicRoute>
-                  <Login />
-                </PublicRoute>
-              }
-            />
+        {/* Route Definitions */}
+        <Routes>
+          {/* Public Route: Only accessible when NOT logged in */}
+          <Route path="/" element={user ? <Navigate to="/userForm" /> : <Navigate to="/login" />} />
+          <Route
+            path="/login"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
 
-            {/* Private Routes: Accessible only when logged in */}
-            <Route
-              path="/userForm"
-              element={
-                <PrivateRoute>
-                  <UserForm />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/editor"
-              element={
-                <PrivateRoute>
-                  <UserEditor />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/profile"
-              element={
-                <PrivateRoute>
-                  <UserProfile />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/counter"
-              element={
-                <PrivateRoute>
-                  <Counter />
-                </PrivateRoute>
-              }
-            />
-          </Routes>
-        </Container>
-      </Router>
-   
+          {/* Private Routes: Accessible only when logged in */}
+          <Route
+            path="/userForm"
+            element={
+              <PrivateRoute>
+                <UserForm />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/editor"
+            element={
+              <PrivateRoute>
+                <UserEditor />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <PrivateRoute>
+                <UserProfile />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/counter"
+            element={
+              <PrivateRoute>
+                <Counter />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
